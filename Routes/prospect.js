abee6c7dd82661ac2fe7baa3007aa6909ad64bc6
[shaikhmd007] <[shaikhmohammad520@gmail.com]>
2022-10-28 11:15:58 +0530

    make it better

--- Routes/prospect.js
+++ Routes/prospect.js
@@ -34,6 +34,15 @@ router.post('/update/:id', async (req, res) => {
     }
 })
 
+router.get('/:id', async function (req, res) {
+    try {
+        const response = await prospect.findById({ _id: req.params.id })
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+})
+
 
 router.delete('/delete/:id', async (req, res) => {
     try {
