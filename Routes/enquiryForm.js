532b33b886c052d5748e465b9fc4b99d43466257
[shaikhmd007] <[shaikhmohammad520@gmail.com]>
2022-11-02 01:00:44 +0530

    make it better

--- Routes/enquiryForm.js
+++ Routes/enquiryForm.js
@@ -12,15 +12,6 @@ router.get('/all', async function (req, res) {
     }
 })
 
-router.get('/:id', async function (req, res) {
-    try {
-        const response = await enquiryForm.findById({ _id: req.params.id })
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ error: err })
-    }
-})
-
 router.post('/create', async (req, res) => {
     try {
         const temp = await new enquiryForm(req.body)
@@ -33,10 +24,22 @@ router.post('/create', async (req, res) => {
     }
 });
 
+
 router.post('/update/:id', async (req, res) => {
     try {
         const response = await enquiryForm.findByIdAndUpdate(req.params.id, req.body);
         return res.status(200).json(response)
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+})
+
+
+
+router.get('/:id', async function (req, res) {
+    try {
+        const response = await enquiryForm.findById({ _id: req.params.id })
+        return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json({ error: err })
     }
