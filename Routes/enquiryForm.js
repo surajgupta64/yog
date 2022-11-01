0ab35c0158941dfbe8ca6b7a62e7e13f723bbde0
[shaikhmd007] <[shaikhmohammad520@gmail.com]>
2022-11-02 01:19:58 +0530

    first commit

--- Routes/enquiryForm.js
+++ Routes/enquiryForm.js
@@ -11,30 +11,16 @@ router.get('/all', async function (req, res) {
         return res.status(500).json({ error: err })
     }
 })
-
 router.post('/create', async (req, res) => {
     try {
         const temp = await new enquiryForm(req.body)
         const response = await temp.save();
-        // const response = await enquiryForm.insertMany(salesclosureData)
         console.log(response);
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json({ error: err })
     }
 });
-
-
-router.post('/update/:id', async (req, res) => {
-    try {
-        const response = await enquiryForm.findByIdAndUpdate(req.params.id, req.body);
-        return res.status(200).json(response)
-    } catch (err) {
-        return res.status(500).json({ error: err })
-    }
-})
-
-
 
 router.get('/:id', async function (req, res) {
     try {
@@ -45,6 +31,14 @@ router.get('/:id', async function (req, res) {
     }
 })
 
+router.post('/update/:id', async (req, res) => {
+    try {
+        const response = await enquiryForm.findByIdAndUpdate(req.params.id, req.body);
+        return res.status(200).json(response)
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+})
 
 router.delete('/delete/:id', async (req, res) => {
     try {
