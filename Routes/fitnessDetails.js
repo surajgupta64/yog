8b378ab269dc9b5fdda46ac26a9b8a29c822eb9f
[shaikhmd007] <[shaikhmohammad520@gmail.com]>
2022-12-13 18:00:48 +0530

    first commit

--- Routes/fitnessDetails.js
+++ Routes/fitnessDetails.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const Invoice = require('../Models/Invoice');
+const fitnessDetails = require('../Models/fitnessDetails');
 const router = express.Router()
 //modelName
 
 router.get('/all', async function (req, res) {
     try {
-        const response = await Invoice.find()
+        const response = await fitnessDetails.find()
         return res.status(200).json(response);
     } catch (err) {
         return res.status(500).json({ error: err })
@@ -14,7 +14,7 @@ router.get('/all', async function (req, res) {
 
 router.post('/create', async (req, res) => {
     try {
-        const temp = await new Invoice(req.body)
+        const temp = await new fitnessDetails(req.body)
         const response = await temp.save();
         console.log(response);
         return res.status(200).json(response);
@@ -23,9 +23,18 @@ router.post('/create', async (req, res) => {
     }
 });
 
+router.get('/:id', async function (req, res) {
+    try {
+        const response = await fitnessDetails.findById({ _id: req.params.id })
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(500).json({ error: err })
+    }
+})
+
 router.post('/update/:id', async (req, res) => {
     try {
-        const response = await Invoice.findByIdAndUpdate(req.params.id, req.body);
+        const response = await fitnessDetails.findByIdAndUpdate(req.params.id, req.body);
         return res.status(200).json(response)
     } catch (err) {
         return res.status(500).json({ error: err })
@@ -33,27 +42,9 @@ router.post('/update/:id', async (req, res) => {
 })
 
 
-router.get('/:id', async function (req, res) {
-    try {
-        const response = await Invoice.findById({ _id: req.params.id })
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ error: err })
-    }
-})
-
-router.get('/invoiceGet/:memberId', async function (req, res) {
-    try {
-        const response = await Invoice.findOne({ MemberId: req.params.memberId })
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ error: err })
-    }
-})
-
 router.delete('/delete/:id', async (req, res) => {
     try {
-        const response = await Invoice.findByIdAndDelete(req.params.id);
+        const response = await fitnessDetails.findByIdAndDelete(req.params.id);
         return res.status(200).json(response)
     } catch (err) {
         return res.status(500).json({ error: err })
