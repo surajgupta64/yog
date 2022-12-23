cec057913500ba721b4f25f558d4eabb7a589b66
Suraj Gupta <72219047+surajgupta64@users.noreply.github.com>
2022-12-24 04:19:11 +0530

    Update staffAttendance.js

--- Models/staffAttendance.js
+++ Models/staffAttendance.js
@@ -3,8 +3,10 @@ let mongoose = require('mongoose')
 
 let staffAttentance = new mongoose.Schema({
     username: String,
+    staffId:String,
     attentanceId: String,
     StaffName: String,
+    staffContact:String,
     centerId: String,
     checkDate: Date,
     checkIn: String,
